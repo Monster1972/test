a3970f093f4405a3a66516084ff3764d18237bf3
dottang <dottang@tencent.com>
2020-04-12 11:40:04 +0800

    测试

---
+++ app/src/test/java/com/example/yamade/test/DogTest.java
@@ -0,0 +1,28 @@
+package com.example.yamade.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DogTest {
+
+    @Test
+    public void getmWeight() {
+
+    }
+
+    @Test
+    public void setmWeight() {
+
+    }
+
+    @Test
+    public void getmNickName() {
+
+    }
+
+    @Test
+    public void setmNickName() {
+
+    }
+}
---
+++ app/src/test/java/com/example/yamade/test/PersonTest.java
@@ -0,0 +1,30 @@
+package com.example.yamade.test;
+
+import org.junit.Before;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PersonTest {
+
+    Person mPerson;
+
+    @Before
+    //运行所有测试用例之前跑一次
+    public void initPerson() {
+        //创建测试对象
+        mPerson = new Person();
+    }
+
+    @org.junit.Test
+    public void testMakeMoney() {
+
+        //调用测试方法
+        int money = mPerson.makeMoney(100);
+
+        //断言判断
+        assertEquals(5000, money);
+
+    }
+
+
+}
